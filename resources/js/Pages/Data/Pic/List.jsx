25eab1416fa753b2227e6aa32440b5977ba03a99
irfandi <irfandi.di122@gmail.com>
2025-04-17 15:28:36 +0700

    feat:admin panel action index data pic

--- resources/js/Pages/Data/Pic/List.jsx
+++ resources/js/Pages/Data/Pic/List.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import { Head, Link } from '@inertiajs/react';
 
 export default function List({ auth, Pic }) {
     return (
@@ -8,24 +8,38 @@ export default function List({ auth, Pic }) {
             <Head title="Daftar PIC" />
 
             <div className="py-8 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 bg-white rounded-xl">
-                <h2 className="text-2xl font-bold mb-4">Daftar PIC</h2>
 
                 <div className="overflow-x-auto">
                     <table className="min-w-full bg-white border border-gray-200 rounded shadow">
                         <thead className="bg-gray-100">
                             <tr>
-                                <th className="px-6 py-3 border-b text-left text-sm font-semibold text-gray-700">ID</th>
-                                <th className="px-6 py-3 border-b text-left text-sm font-semibold text-gray-700">Nama</th>
-                                <th className="px-6 py-3 border-b text-left text-sm font-semibold text-gray-700">Jabatan</th>
+                                <th className="px-6 py-3 border-b text-sm font-semibold text-center text-gray-700">PIC ADMIN</th>
+                                <th className="px-6 py-3 border-b text-sm font-semibold text-center text-gray-700">PIC PROJECT</th>
+                                <th className="px-6 py-3 border-b text-sm font-semibold text-center text-gray-700">WASPANG TA</th>
+                                {auth.user &&(
+                                <th className="px-6 py-3 border-b text-sm font-semibold text-center text-gray-700">ACTION</th>
+                                )}
                             </tr>
                         </thead>
                         <tbody>
                             {Pic.length > 0 ? (
                                 Pic.map((item) => (
                                     <tr key={item.id} className="hover:bg-gray-50">
-                                        <td className="px-6 py-4 border-b text-sm text-gray-700">{item.id}</td>
-                                        <td className="px-6 py-4 border-b text-sm text-gray-700">{item.nama}</td>
-                                        <td className="px-6 py-4 border-b text-sm text-gray-700">{item.jabatan}</td>
+                                        <td className="px-6 py-4 border-b text-sm text-gray-700">{item.pic_admin}</td>
+                                        <td className="px-6 py-4 border-b text-sm text-gray-700">{item.pic_project}</td>
+                                        <td className="px-6 py-4 border-b text-sm text-gray-700">{item.waspang_ta}</td>
+                                        {auth.user && (
+                                        <td className="px-6 py-4 border-b text-sm text-gray-700">
+                                            <Link
+                                                href={route('data.pic.index',item.id)}
+                                                >Edit PIC
+                                            </Link>
+                                            <Link
+                                                href={route('data.pic.index',item.id)}
+                                                >Hapus PIC
+                                            </Link>
+                                        </td>
+                                        )}
                                     </tr>
                                 ))
                             ) : (

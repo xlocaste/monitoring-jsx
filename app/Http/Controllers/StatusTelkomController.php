62b236fa8fd3484f4cb0483cb08d26bf0bfbce8a
irfandi <irfandi.di122@gmail.com>
2025-04-27 19:03:44 +0700

    feat:import redirect status telkom controller

--- app/Http/Controllers/StatusTelkomController.php
+++ app/Http/Controllers/StatusTelkomController.php
@@ -11,6 +11,7 @@ use App\Models\StatusRekonMitra;
 use App\Models\StatusBastTelkom;
 use App\Http\Requests\StatusTelkom\StoreRequest;
 use App\Http\Requests\StatusTelkom\UpdateRequest;
+use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
 
 class StatusTelkomController extends Controller

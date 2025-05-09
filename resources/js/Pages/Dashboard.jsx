3b0ad8facebe989b08a09752e54edb1e49ac594d
xlocaste <irfandi.di122@gmail.com>
2025-05-09 12:35:54 +0700

    fix:size count table dashboard

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -18,36 +18,36 @@ export default function Dashboard({ auth, totalPic, totalMitra, totalProject, to
                     <div className="absolute -bottom-14 -right-10 opacity-30 text-[12rem] pointer-events-none">
                         <CgProfile />
                     </div>
-                    <h3 className="text-4xl text-gray-700 font-bold relative z-10">PIC</h3>
-                    <p className="text-4xl font-bold text-gray-700 relative z-10">{totalPic}</p>
+                    <h3 className="text-2xl text-gray-700 font-bold relative z-10">PIC</h3>
+                    <p className="text-2xl font-bold text-gray-700 relative z-10">{totalPic}</p>
                 </div>
                 <div className="relative bg-gradient-to-br from-red-700 via-red-400 to-white p-6 rounded shadow text-gray-900 overflow-hidden">
                     <div className="absolute -bottom-20 -right-10 opacity-30 text-[12rem] pointer-events-none">
                         <BsFillClipboard2DataFill />
                     </div>
-                    <h3 className="text-4xl text-gray-700 font-bold relative z-10">Mitra</h3>
-                    <p className="text-4xl font-bold text-gray-700 relative z-10">{totalMitra}</p>
+                    <h3 className="text-2xl text-gray-700 font-bold relative z-10">Mitra</h3>
+                    <p className="text-2xl font-bold text-gray-700 relative z-10">{totalMitra}</p>
                 </div>
                 <div className="relative bg-gradient-to-br from-red-700 via-red-400 to-white p-6 rounded shadow text-gray-900 overflow-hidden">
                     <div className="absolute -bottom-14 -right-10 opacity-30 text-[12rem] pointer-events-none">
                         <FaProjectDiagram />
                     </div>
-                    <h3 className="text-4xl text-gray-700 font-bold relative z-10">Project</h3>
-                    <p className="text-4xl font-bold text-gray-700 relative z-10">{totalProject}</p>
+                    <h3 className="text-2xl text-gray-700 font-bold relative z-10">Project</h3>
+                    <p className="text-2xl font-bold text-gray-700 relative z-10">{totalProject}</p>
                 </div>
                 <div className="relative bg-gradient-to-br from-red-700 via-red-400 to-white p-6 rounded shadow text-gray-900 overflow-hidden">
                     <div className="absolute -bottom-20 -right-10 opacity-30 text-[12rem] pointer-events-none">
                         <AiFillDatabase />
                     </div>
-                    <h3 className="text-4xl text-gray-700 font-bold relative z-10">Status Mitra</h3>
-                    <p className="text-4xl font-bold text-gray-700 relative z-10">{totalStatusMitra}</p>
+                    <h3 className="text-2xl text-gray-700 font-bold relative z-10">Status Mitra</h3>
+                    <p className="text-2xl font-bold text-gray-700 relative z-10">{totalStatusMitra}</p>
                 </div>
                 <div className="relative bg-gradient-to-br from-red-700 via-red-400 to-white p-6 rounded shadow text-gray-900 overflow-hidden">
                     <div className="absolute -bottom-20 -right-10 opacity-30 text-[12rem] pointer-events-none">
                         <AiFillDatabase />
                     </div>
-                    <h3 className="text-4xl text-gray-700 font-bold relative z-10">Status Telkom</h3>
-                    <p className="text-4xl font-bold text-gray-700 relative z-10">{totalStatusTelkom}</p>
+                    <h3 className="text-2xl text-gray-700 font-bold relative z-10">Status Telkom</h3>
+                    <p className="text-2xl font-bold text-gray-700 relative z-10">{totalStatusTelkom}</p>
                 </div>
             </div>
         </AuthenticatedLayout>

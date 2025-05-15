fca41ed33898906d0782d33b6eb39aa949730045
xlocaste <irfandi.di122@gmail.com>
2025-05-15 21:19:39 +0700

    feat:dashboard styling

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,35 +1,46 @@
 import { Link, Head } from '@inertiajs/react';
+import PrimaryButton from '@/Components/PrimaryButton';
+import ApplicationLogo from '@/Components/ApplicationLogo';
 
 export default function Welcome({ auth, laravelVersion, phpVersion }) {
     return (
         <>
             <Head title="Welcome" />
-            <div className="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-                <div className="sm:fixed sm:top-0 sm:right-0 p-6 text-end">
-                    {auth.user ? (
-                        <Link
-                            href={route('dashboard')}
-                            className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                        >
-                            Dashboard
-                        </Link>
-                    ) : (
-                        <>
-                            <Link
-                                href={route('login')}
-                                className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                            >
-                                Log in
-                            </Link>
-
-                            <Link
-                                href={route('register')}
-                                className="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                            >
-                                Register
-                            </Link>
-                        </>
-                    )}
+            <div className="relative min-h-screen">
+                <img
+                    src="/images/background dashboard.jpg"
+                    alt="Background"
+                    className="absolute inset-0 w-full h-full object-cover brightness-50"
+                    style={{ objectPosition: '5% 100%' }}
+                />
+                <div className="fixed inset-0 flex items-center justify-center text-center">
+                    <div className='bg-white/30 px-14 py-4 rounded-lg border w-96'>
+                        <div className='flex flex-col space-y-4'>
+                            <div className=''>
+                                <p className='text-white text-xl font-bold'>Selamat Datang di Website</p>
+                                <p className='text-white text-sm'>
+                                    Aplikasi Monitoring Telkom Akses ini dirancang untuk memudahkan pemantauan dan pengelolaan jaringan serta layanan Telkom secara real-time. Dengan fitur lengkap dan antarmuka yang user-friendly, Anda dapat memantau performa jaringan, status perangkat, serta laporan gangguan dengan cepat dan akurat.
+                                </p>
+                            </div>
+                            {auth.user ? (
+                                <Link
+                                    href={route('dashboard')}
+                                >
+                                    <PrimaryButton>
+                                        Dashboard
+                                    </PrimaryButton>
+                                </Link>
+                            ) : (
+                                    <Link
+                                        href={route('login')}
+                                    >
+                                        <PrimaryButton>
+                                            Log in
+                                        </PrimaryButton>
+                                    </Link>
+                            )}
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
